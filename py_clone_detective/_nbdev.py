125099b14f5c7260a8bc0aa67c4a6162b19b1ee9
morriso1 <ottomorris@gmail.com>
2021-08-01 20:19:37 +0100

    added clone_counter classes

--- py_clone_detective/_nbdev.py
+++ py_clone_detective/_nbdev.py
@@ -16,9 +16,13 @@ index = {"clean_img_names": "00_utils.ipynb",
          "generate_touch_counting_image": "00_utils.ipynb",
          "label_clones_output_unmerged_and_merged": "00_utils.ipynb",
          "get_all_labeled_clones_unmerged_and_merged": "00_utils.ipynb",
-         "update_1st_coord_and_dim_of_xarr": "00_utils.ipynb"}
+         "update_1st_coord_and_dim_of_xarr": "00_utils.ipynb",
+         "CloneCounter": "01_clone_counters.ipynb",
+         "LazyCloneCounter": "01_clone_counters.ipynb",
+         "PersistentCloneCounter": "01_clone_counters.ipynb"}
 
-modules = ["utils.py"]
+modules = ["utils.py",
+           "clone_counters.py"]
 
 doc_url = "https://morriso1.github.io/py_clone_detective/"
 
