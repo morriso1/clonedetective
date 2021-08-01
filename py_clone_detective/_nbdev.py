2e9fa41501ac9b4e0ec6c10e7a609dbc0ccba5e9
morriso1 <ottomorris@gmail.com>
2021-08-01 20:10:31 +0100

    added utils functions

--- py_clone_detective/_nbdev.py
+++ py_clone_detective/_nbdev.py
@@ -6,9 +6,17 @@ index = {"clean_img_names": "00_utils.ipynb",
          "check_lists_identical": "00_utils.ipynb",
          "img_path_to_xarr": "00_utils.ipynb",
          "last2dims": "00_utils.ipynb",
+         "check_channels_input_suitable_and_return_channels": "00_utils.ipynb",
          "extend_region_properties_list": "00_utils.ipynb",
          "add_scale_regionprops_table_area_measurements": "00_utils.ipynb",
-         "lazy_props": "00_utils.ipynb"}
+         "lazy_props": "00_utils.ipynb",
+         "reorder_df_to_put_ch_info_first": "00_utils.ipynb",
+         "region_overlap": "00_utils.ipynb",
+         "calculate_overlap": "00_utils.ipynb",
+         "generate_touch_counting_image": "00_utils.ipynb",
+         "label_clones_output_unmerged_and_merged": "00_utils.ipynb",
+         "get_all_labeled_clones_unmerged_and_merged": "00_utils.ipynb",
+         "update_1st_coord_and_dim_of_xarr": "00_utils.ipynb"}
 
 modules = ["utils.py"]
 
